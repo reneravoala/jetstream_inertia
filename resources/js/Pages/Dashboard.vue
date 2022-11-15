bcc7dbf469ff3faa410eb6adf75f9005363dc2ee
Rene R <reneravoala2@gmail.com>
2022-11-15 10:11:00 +0300

    first commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,49 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';
+import Calendar from 'primevue/calendar';
+
+const calendar = null;
+
+const dateChanged = (d) => {
+    alert(d.valueOf());
+}
+
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <div class="flex justify-between">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    Dashboard
+                </h2>
+                <Calendar v-model="calendar" :inline="true" @date-select="dateChanged">
+                    <template #date="slotProps">
+                        <span v-if="slotProps.date.day > 10 && slotProps.date.day < 15"
+                              class="w-full h-full text-center bg-gray-400 font-bold leading-9">{{ slotProps.date.day }}</span>
+                        <template v-else>{{ slotProps.date.day }}</template>
+                    </template>
+                </Calendar>
+            </div>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <Welcome/>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<style>
+.p-datepicker {
+    @apply !w-fit;
+}
+
+.p-datepicker-calendar > tbody > tr > td {
+    @apply p-6;
+}
+</style>
---
+++ resources/js/Pages/Teams/Partials/UpdateTeamNameForm.vue
@@ -0,0 +1,159 @@
+<script setup>
+import { useForm } from '@inertiajs/inertia-vue3';
+import ActionMessage from '@/Components/ActionMessage.vue';
+import FormSection from '@/Components/FormSection.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue'
+import {Inertia} from "@inertiajs/inertia";
+import {ref} from "vue";
+
+const props = defineProps({
+    team: Object,
+    permissions: Object,
+});
+
+const form = useForm({
+    name: props.team.name,
+});
+
+const updateTeamName = () => {
+    form.put(route('teams.update', props.team), {
+        errorBag: 'updateTeamName',
+        preserveScroll: true,
+    });
+};
+
+const photoPreview = ref(null);
+const photoInput = ref(null);
+const selectNewPhoto = () => {
+    photoInput.value.click();
+};
+
+const updatePhotoPreview = () => {
+    const photo = photoInput.value.files[0];
+
+    if (! photo) return;
+
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+        photoPreview.value = e.target.result;
+    };
+
+    reader.readAsDataURL(photo);
+};
+
+const deletePhoto = () => {
+    Inertia.delete(route('current-user-photo.destroy'), {
+        preserveScroll: true,
+        onSuccess: () => {
+            photoPreview.value = null;
+            clearPhotoFileInput();
+        },
+    });
+};
+
+const clearPhotoFileInput = () => {
+    if (photoInput.value?.value) {
+        photoInput.value.value = null;
+    }
+};
+</script>
+
+<template>
+    <FormSection @submitted="updateTeamName">
+        <template #title>
+            Team Name
+        </template>
+
+        <template #description>
+            The team's name and owner information.
+        </template>
+
+        <template #form>
+            <!-- Profile Photo -->
+            <div class="col-span-6 sm:col-span-4">
+                <!-- Profile Photo File Input -->
+                <input
+                    ref="photoInput"
+                    type="file"
+                    class="hidden"
+                    @change="updatePhotoPreview"
+                >
+
+                <InputLabel for="photo" value="Logo" />
+
+                <!-- Current Profile Photo -->
+                <div v-show="! photoPreview" class="mt-2">
+                    <img :src="team.profile_photo_url" :alt="team.name" class="rounded h-20 w-20 object-cover">
+                </div>
+
+                <!-- New Profile Photo Preview -->
+                <div v-show="photoPreview" class="mt-2">
+                    <span
+                        class="block rounded w-20 h-20 bg-cover bg-no-repeat bg-center"
+                        :style="'background-image: url(\'' + photoPreview + '\');'"
+                    />
+                </div>
+
+                <SecondaryButton class="mt-2 mr-2" type="button" @click.prevent="selectNewPhoto">
+                    Nouveau logo
+                </SecondaryButton>
+
+                <SecondaryButton
+                    v-if="team.profile_photo_path"
+                    type="button"
+                    class="mt-2"
+                    @click.prevent="deletePhoto"
+                >
+                    Supprimer le logo
+                </SecondaryButton>
+
+                <InputError :message="form.errors.photo" class="mt-2" />
+            </div>
+            <!-- Team Owner Information -->
+            <div class="col-span-6">
+                <InputLabel value="Team Owner" />
+
+                <div class="flex items-center mt-2">
+                    <img class="w-12 h-12 rounded-full object-cover" :src="team.owner.profile_photo_url" :alt="team.owner.name">
+
+                    <div class="ml-4 leading-tight">
+                        <div>{{ team.owner.name }}</div>
+                        <div class="text-gray-700 text-sm">
+                            {{ team.owner.email }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Team Name -->
+            <div class="col-span-6 sm:col-span-4">
+                <InputLabel for="name" value="Team Name" />
+
+                <TextInput
+                    id="name"
+                    v-model="form.name"
+                    type="text"
+                    class="mt-1 block w-full"
+                    :disabled="! permissions.canUpdateTeam"
+                />
+
+                <InputError :message="form.errors.name" class="mt-2" />
+            </div>
+        </template>
+
+        <template v-if="permissions.canUpdateTeam" #actions>
+            <ActionMessage :on="form.recentlySuccessful" class="mr-3">
+                Saved.
+            </ActionMessage>
+
+            <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                Save
+            </PrimaryButton>
+        </template>
+    </FormSection>
+</template>
